fbf9d75b77e4531f08eeea765bacb1d472c06146
OleksiyBodnia <90392804+OleksiyBodnia@users.noreply.github.com>
2024-03-14 20:30:02 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import './js/header';
 import './js/hero';
 import './js/about-me';
 import './js/benefits';
-import './js/projects';
+import '/js/projects';
 import './js/faq';
 import './js/covers';
 import './js/work-together';
