a00c99f633d966e4aae0f1069879cdb53e5fddb6
Kateryna <kati.skrinnikova@gmail.com>
2024-03-17 11:48:04 +0200

    accordion ready

--- src/js/about-me.js
+++ src/js/about-me.js
@@ -37,40 +37,5 @@ const swiperAbout = new Swiper('.swiper-about', {
   //   simulateTouch: true,
 });
 
-const accordion = new Accordion('.accordion-container', {});
-accordion.open(1);
-
-const buttonUp = document.querySelector('.about-me-button-up');
-const buttonUpAll = document.querySelectorAll('.about-me-button-up');
-
-const buttonDown = document.querySelector('.about-me-button-down');
-const buttonDownAll = document.querySelectorAll('.about-me-button-down');
-
-const accordionPanelAll = document.querySelectorAll('.ac-panel');
-const accordionPanel = document.querySelector('.ac-panel');
-
-// accordionPanelAll.forEach(item => {
-//   return item.classList.add('visually-hidden');
-// });
-
-buttonUp.addEventListener('click', visiblePanel);
-buttonDown.addEventListener('click', invisiblePanel);
-
-accordionPanel.classList.add('visually-hidden');
-buttonDown.classList.add('is-hidden');
-
-function visiblePanel() {
-  buttonDown.classList.remove('is-hidden');
-  buttonUp.classList.add('is-hidden');
-  accordionPanel.classList.remove('visually-hidden');
-}
-
-function invisiblePanel() {
-  buttonDown.classList.add('is-hidden');
-  buttonUp.classList.remove('is-hidden');
-  accordionPanel.classList.add('visually-hidden');
-}
-
-// buttonDownAll.forEach(item => {
-//   return item.classList.add('is-hidden');
-// });
+const accordionAbout = new Accordion('.accordion-container-about');
+accordion.open(0);
