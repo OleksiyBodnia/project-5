b11da0d4eb9687ffd90e03a3fca4b5407090beec
Oleksiy Bodnia <bodnjaal@gmail.com>
2024-03-17 19:17:01 +0200

    Update faq.js

--- src/js/faq.js
+++ src/js/faq.js
@@ -7,16 +7,15 @@ const accordionfaq = new faqAccordion('.accordion-container-faq', {
 });
 
 const faqButtons = document.querySelectorAll('.faq-show-hidden-text-button');
-console.log(faqButtons)
 
 faqButtons.forEach(button => {
     button.addEventListener('click', () => {
         const useElement = button.querySelector('use');
         const currentHref = useElement.getAttribute('href');
         if (currentHref === './icons/sprite.svg#arrow-down') {
-            useElement.setAttribute('href', './icons/sprite.svg#arrow-up');
+            useElement.setAttribute('href', '../icons/sprite.svg#arrow-up');
         } else {
-            useElement.setAttribute('href', './icons/sprite.svg#arrow-down');
+            useElement.setAttribute('href', '../icons/sprite.svg#arrow-down');
         }
 
     })
