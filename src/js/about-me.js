6c8bd4069e730d25d5449a7e1a039f5ad933c154
Oleksiy Bodnia <bodnjaal@gmail.com>
2024-03-17 17:11:30 +0200

    update about-me swiper

--- src/js/about-me.js
+++ src/js/about-me.js
@@ -1,68 +1,134 @@
+// import Accordion from 'accordion-js';
+// import 'accordion-js/dist/accordion.min.css';
+
+// import Swiper from 'swiper/bundle';
+// import 'swiper/css/bundle';
+
+// const swiperAbout = new Swiper('.swiper-about', {
+//   direction: 'horizontal',
+//   loop: true,
+//   speed: 750,
+
+//   navigation: {
+//     nextEl: '.about-me-skills-button',
+//   },
+
+//   breakpoints: {
+//     // when window width is >= 320px
+//     320: {
+//       slidesPerView: 2,
+//       spaceBetween: 0,
+//     },
+//     // when window width is >= 678px
+//     678: {
+//       slidesPerView: 3,
+//     },
+//     // when window width is >= 1440px
+//     1440: {
+//       slidesPerView: 6,
+//     },
+//   },
+
+//   keyboard: {
+//     enabled: true,
+//   },
+
+//   grabCursor: true,
+//   simulateTouch: true,
+
+//   on: {
+//     init: function () {
+//       const activeIndex = this.activeIndex;
+//       this.slides.forEach((slide, index) => {
+//         if (index === activeIndex) {
+//           slide[index].classList.remove('active-slide');
+//           slide[index + 1].classList.add('active-slide');
+//         } else {
+//           slide.classList.remove('active-slide');
+//         }
+//       });
+//     },
+//   },
+
+//   slideChange: function () {
+//     const activeIndex = this.activeIndex;
+//     this.slides.forEach((slide, index) => {
+//       if (index === activeIndex) {
+//         slide[index].classList.remove('active-slide');
+//         slide[index + 1].classList.add('active-slide');
+//       } else {
+//         slide.classList.remove('active-slide');
+//       }
+//     });
+//   },
+// });
+// swiperAbout.update();
+
+// import Swiper bundle with all modules installed
+import Swiper from 'swiper/bundle';
+
+// import styles bundle
+import 'swiper/css/bundle';
 import Accordion from 'accordion-js';
 import 'accordion-js/dist/accordion.min.css';
+const slideContainer = document.querySelector(".container");
 
-import Swiper from 'swiper/bundle';
-import 'swiper/css/bundle';
+function countSkillsPerView(){
+    const containerWidth = slideContainer.clientWidth;
+    if (containerWidth === 375){
+      return 2;
+    } else if(containerWidth === 768){
+      return 3;
+    } else if(containerWidth === 1440) {
+      return 6;
+    }
+  }
 
-const swiperAbout = new Swiper('.swiper-about', {
-  direction: 'horizontal',
-  loop: true,
-  speed: 750,
-
-  navigation: {
-    nextEl: '.about-me-skills-button',
-  },
-
-  breakpoints: {
-    // when window width is >= 320px
-    320: {
-      slidesPerView: 2,
-      spaceBetween: 0,
+const swiper3 = new Swiper(".swiper3", {
+    slidesPerView: countSkillsPerView(),
+    loop: true,
+    navigation: {
+      nextEl: ".right-btn3",
     },
-    // when window width is >= 678px
-    678: {
-      slidesPerView: 3,
-    },
-    // when window width is >= 1440px
-    1440: {
-      slidesPerView: 6,
-    },
-  },
+    speed: 750,
+    keyboard: {
+        enabled: true,
+      },
+    
+      grabCursor: true, 
+    
+      simulateTouch: true,
+      on: {
+        init: function () {
+          const activeIndex = this.activeIndex;
+          this.slides.forEach((slide, index) => {
+            if (index === activeIndex) {
+              slide.classList.add("active-slide");
+            } else {
+              slide.classList.remove("active-slide");
+            }
+          });
+        },
+        slideChange: function () {
+          const activeIndex = this.activeIndex;
+          this.slides.forEach((slide, index) => {
+            if (index === activeIndex) {
+              slide.classList.add("active-slide");
+            } else {
+              slide.classList.remove("active-slide");
+            }
+          });
+        },
+      },
+    
+  });
+  function resizeHandle() {
+    swiper3.params.slidesPerView = countSkillsPerView(); 
+    swiper3.update();
+  }
+  window.addEventListener('resize', resizeHandle);
+  
 
-  keyboard: {
-    enabled: true,
-  },
-
-  grabCursor: true,
-  simulateTouch: true,
-
-  on: {
-    init: function () {
-      const activeIndex = this.activeIndex;
-      this.slides.forEach((slide, index) => {
-        if (index === activeIndex) {
-          slide[index].classList.remove('active-slide');
-          slide[index + 1].classList.add('active-slide');
-        } else {
-          slide.classList.remove('active-slide');
-        }
-      });
-    },
-  },
-
-  slideChange: function () {
-    const activeIndex = this.activeIndex;
-    this.slides.forEach((slide, index) => {
-      if (index === activeIndex) {
-        slide[index].classList.remove('active-slide');
-        slide[index + 1].classList.add('active-slide');
-      } else {
-        slide.classList.remove('active-slide');
-      }
-    });
-  },
-});
-swiperAbout.update();
 const accordionAbout = new Accordion('.accordion-container-about', {
   duration: 700,
   showMultiple: true,
