bcc27158e657c35e9508ffa398473c091c39142a
Oleksiy Bodnia <bodnjaal@gmail.com>
2024-03-18 13:20:02 +0200

    add pre finished updates

--- src/js/benefits.js
+++ src/js/benefits.js
@@ -7,7 +7,7 @@
 // }
 
 const benefitsButton = document.querySelector('.benefits-button');
-const workTogetherSection = document.getElementById('work-together-section');
+const workTogetherSection = document.getElementById('work-together-section-id');
 
 benefitsButton.addEventListener('click', () => {
   workTogetherSection.scrollIntoView({ behavior: 'smooth' });
