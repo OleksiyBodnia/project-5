0ac24b0a4a8708fe6c3f7b5a9de52af8d5b2d112
Oleksiy Bodnia <bodnjaal@gmail.com>
2024-03-19 23:54:30 +0200

    update benefits

--- src/js/benefits.js
+++ src/js/benefits.js
@@ -1,16 +1,6 @@
-// import Swiper from 'swiper/bundle';
-// import 'swiper/css/bundle';
+const benefitsButton = document.querySelector('.benefits-button');
+const workTogetherSection = document.getElementById('work-together-section-id');
 
-// function scrollToWorkTogether() {
-//   const workTogetherSection = document.getElementById("work-together-section");
-//   workTogetherSection.scrollIntoView({ behavior: "smooth" });
-// }
-
-
-// працюэ для кнопки (<!-- <button type="button" class="benefits-button">Order the project</button> -->)
-// const benefitsButton = document.querySelector('.benefits-button');
-// const workTogetherSection = document.getElementById('work-together-section-id');
-
-// benefitsButton.addEventListener('click', () => {
-//   workTogetherSection.scrollIntoView({ behavior: 'smooth' });
-// });
+benefitsButton.addEventListener('click', () => {
+  workTogetherSection.scrollIntoView({ behavior: 'smooth' });
+});
